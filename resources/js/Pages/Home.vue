95c5a11dd125e021967664cd4721039b23cad482
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-04-26 16:38:06 +0200

    Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello, {{ name }}</h1>
+
+        <p>In this series we will use the following frameworks:</p>
+
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+}
+</script>
