c1f49cc8a3a3ed79929403fb16a10a0ea2d4b096
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-05-05 13:33:12 +0200

    Default Layouts & Global Component Registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,28 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
+import Layout from "./Shared/Layout";
 
 InertiaProgress.init({
   showSpinner: true,
 })
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
+  resolve: name => {
+    let page = require(`./Pages/${name}`).default;
+
+    /*if (!page.layout) {
+      page.layout = Layout
+    }*/
+
+    page.layout ??= Layout;
+
+    return page;
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .component("Link", Link)
       .mount(el)
   },
 })
