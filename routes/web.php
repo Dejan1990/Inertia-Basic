cffda58f9d50af7ce3b0f9876e12e3aac462c71f
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-05-06 16:20:19 +0200

    Filtering, State & Query Strings

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Models\User;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 
 Route::get('/', function () {
     //return Inertia::render('Welcome');
@@ -12,10 +13,17 @@ Route::get('/', function () {
 Route::get('/users', function () {
     //sleep(2);
     return inertia('Users', [
-        'users' => User::paginate(10)->through(fn($user) => [ //with map() we get a new collection, through is almost the same as map, but it's applied to current slice of items
-            'id' => $user->id,
-            'name' => $user->name
-        ])
+        'users' => User::query()
+            ->when(Request::input('search'), function ($query, $search) { // $search = Request::input('search')
+                $query->where('name', 'like', "%{$search}%");
+            })
+            ->paginate()
+            ->withQueryString()
+            ->through(fn($user) => [ //with map() we get a new collection, through is almost the same as map, but it's applied to current slice of items
+                'id' => $user->id,
+                'name' => $user->name
+            ]),
+        'filters' => Request::only(['search'])
     ]);
 });
 
@@ -26,3 +34,4 @@ Route::get('/settings', function () {
 Route::post('/logout', function () {
     dd(request('foo'));
 });
+//with map() we get a new collection, through is almost the same as map, but it's applied to current slice of items
